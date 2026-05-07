4a10c84583fec7c3e9d79bc8a62de367512b52dd
Mohd-Umair-Lari <umairlari0786@gmail.com>
2026-05-08 00:23:15 +0530

    routed the backend from render to HuggingFace for better uptime

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,10 +1,13 @@
-// Production URL configuration
 const isDev = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
 // Backend URLs
+
+
 const DEV_URL = "http://localhost:5000";
-// 🟢 Connected to your Hugging Face Backend URL
+// Connected to your Hugging Face Backend URL
 const PRODUCTION_URL = "https://umairlari-ai-financial-advisor-backend.hf.space";
+// Connection url to the Render backend
+const RENDER_BACKEND_URL = "https://ai-driven-invest-planner.onrender.com"
 
 export const BACKEND_URL = isDev ? DEV_URL : PRODUCTION_URL;
 
