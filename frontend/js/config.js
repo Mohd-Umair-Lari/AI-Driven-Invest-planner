bfcbc7261df65e526d9bd8058009a434943c2f11
Mohd-Umair-Lari <umairlari0786@gmail.com>
2026-05-09 14:00:58 +0530

    fixed the graph for the Cash Flow

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -6,8 +6,8 @@ const isDev = window.location.hostname === "localhost" || window.location.hostna
 const DEV_URL = "http://localhost:5000";
 // Connected to your Hugging Face Backend URL
 const PRODUCTION_URL = "https://umairlari-ai-financial-advisor-backend.hf.space";
+// const PRODUCTION_URL = "https://ai-driven-invest-planner.onrender.com"
 // Connection url to the Render backend
-// const RENDER_BACKEND_URL = "https://ai-driven-invest-planner.onrender.com"
 
 export const BACKEND_URL = isDev ? DEV_URL : PRODUCTION_URL;
 
